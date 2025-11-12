e489e8a3c1f16c6e2d4298fd8512909a0cf62762
Manish Kumar <manishkumar9576700@gmail.com>
2025-11-13 00:08:09 +0530

    bgm and game over audio added and game over page added

--- script.js
+++ script.js
@@ -1,175 +1,241 @@
-const board = document.querySelector('main .bottom');
-const side = 50;
+window.addEventListener("DOMContentLoaded", () => {
+    const board = document.querySelector('main .bottom');
+    const side = 50;
+    const bgm = document.querySelector(".bgm");
+    bgm.load();
+    const over = document.querySelector(".game-over")
+    over.load();
+    let rows = Math.floor(board.clientHeight / side);
+    let cols = Math.floor(board.clientWidth / side);
+    const score = document.querySelector("#score");
+    let scr = 0;
 
-let snake = [{ x: 1, y: 3 }];
-let food = null
-let direction = 'right';
+    let snake = [{ x: 1, y: 3 }];
+    let food = null
+    let direction = 'right';
 
-const foodImg = ["./img/rahul2.jpg", "./img/mamta.png", "./img/arvind.jpeg","./img/lallu.png","./img/tejpratap.jpg"];
+    const foodImg = ["./img/rahul2.jpg", "./img/mamta.png", "./img/arvind.jpeg", "./img/lallu.png", "./img/tejpratap.jpg"];
 
-window.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowUp' && direction !== 'down') {
-        direction = 'up';
-    }
-
-
-    else if (e.key === 'ArrowDown' && direction !== 'up') {
-        direction = 'down';
-    }
-    else if (e.key === 'ArrowLeft' && direction !== 'right') {
-        direction = 'left';
-    }
-    else if (e.key === 'ArrowRight' && direction !== 'left') {
-        direction = 'right';
-    }
-});
-function createBlocks() {
-
-    if (board.innerHTML !== '') {
-        board.innerHTML = '';
-    }
-    const rows = Math.floor(board.clientHeight / side);
-    const cols = Math.floor(board.clientWidth / side);
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'ArrowUp' && direction !== 'down') {
+            direction = 'up';
+        }
 
 
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('block');
-            cell.setAttribute('data-row', `${i}`);
-            cell.setAttribute('data-col', `${j}`);
-            board.appendChild(cell);
+        else if (e.key === 'ArrowDown' && direction !== 'up') {
+            direction = 'down';
+        }
+        else if (e.key === 'ArrowLeft' && direction !== 'right') {
+            direction = 'left';
+        }
+        else if (e.key === 'ArrowRight' && direction !== 'left') {
+            direction = 'right';
+        }
+    });
+    function createBlocks() {
+
+        if (board.innerHTML !== '') {
+            board.innerHTML = '';
+        }
+        rows = Math.floor(board.clientHeight / side);
+        cols = Math.floor(board.clientWidth / side);
+
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('block');
+                cell.setAttribute('data-row', `${i}`);
+                cell.setAttribute('data-col', `${j}`);
+                board.appendChild(cell);
+            }
         }
     }
-}
-createBlocks();
-window.addEventListener('resize', () => {
     createBlocks();
+    window.addEventListener('resize', () => {
+        createBlocks();
+        drawSnake()
+    });
+
+
+    function drawSnake() {
+        const cells = document.querySelectorAll('.block');
+        snake.forEach((part) => {
+            cells.forEach((cell) => {
+                if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
+                    cell.classList.add('snake');
+                }
+            })
+        })
+    }
     drawSnake()
-});
 
-
-function drawSnake() {
-    const cells = document.querySelectorAll('.block');
-    snake.forEach((part) => {
-        cells.forEach((cell) => {
-            if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
-                cell.classList.add('snake');
+    function foodGenerator() {
+        const cells = document.querySelectorAll('.block');
+        let foodX = Math.floor(Math.random() * (board.clientHeight / side));
+        let foodY = Math.floor(Math.random() * (board.clientWidth / side));
+        const foodImgUrl = foodImg[Math.floor(Math.random() * foodImg.length)];
+        snake.forEach((part) => {
+            if (part.x == foodX && part.y == foodY) {
+                foodX = Math.floor(Math.random() * (board.clientHeight / side));
+                foodY = Math.floor(Math.random() * (board.clientWidth / side));
             }
         })
-    })
-}
-drawSnake()
+        cells.forEach((cell) => {
+            if (cell.getAttribute('data-row') == foodX && cell.getAttribute('data-col') == foodY) {
+                cell.classList.add('food');
+                document.querySelector('.food').style.backgroundImage = `url(${foodImgUrl})`;
+                food = { x: foodX, y: foodY };
+            }
+        })
+    }
 
-function foodGenerator() {
-    const cells = document.querySelectorAll('.block');
-    let foodX = Math.floor(Math.random() * (board.clientHeight / side));
-    let foodY = Math.floor(Math.random() * (board.clientWidth / side));
-    const foodImgUrl = foodImg[Math.floor(Math.random() * foodImg.length)];
-    snake.forEach((part) => {
-        if (part.x == foodX && part.y == foodY) {
-            foodX = Math.floor(Math.random() * (board.clientHeight / side));
-            foodY = Math.floor(Math.random() * (board.clientWidth / side));
+    function moveSnake() {
+        const cells = document.querySelectorAll('.block');
+        let head = null;
+        if (direction === 'right') {
+            head = { x: snake[0].x, y: snake[0].y + 1 };
+            snake.forEach((part) => {
+                cells.forEach((cell) => {
+                    if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
+                        cell.classList.remove('snake');
+
+                    }
+                })
+            })
         }
-    })
-    cells.forEach((cell) => {
-        if (cell.getAttribute('data-row') == foodX && cell.getAttribute('data-col') == foodY) {
-            cell.classList.add('food');
-            document.querySelector('.food').style.backgroundImage = `url(${foodImgUrl})`;
-            food = { x: foodX, y: foodY };
+
+        else if (direction === 'left') {
+            head = { x: snake[0].x, y: snake[0].y - 1 };
+            snake.forEach((part) => {
+                cells.forEach((cell) => {
+                    if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
+                        cell.classList.remove('snake');
+                    }
+                })
+            })
         }
+
+        if (direction === 'up') {
+            head = { x: snake[0].x - 1, y: snake[0].y };
+            snake.forEach((part) => {
+                cells.forEach((cell) => {
+                    if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
+                        cell.classList.remove('snake');
+                    }
+                })
+            })
+        }
+
+        else if (direction === 'down') {
+            head = { x: snake[0].x + 1, y: snake[0].y };
+            snake.forEach((part) => {
+                cells.forEach((cell) => {
+                    if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
+                        cell.classList.remove('snake');
+                    }
+                })
+            })
+        }
+
+        snake.unshift(head);
+        snake.pop();
+
+    }
+
+    function clearFood() {
+        const cells = document.querySelectorAll('.block');
+        cells.forEach((cell) => {
+            if (cell.getAttribute('data-row') == food.x && cell.getAttribute('data-col') == food.y) {
+                document.querySelector('.food').style.backgroundImage = "";
+                scr += 10;
+                score.textContent = scr;
+                cell.classList.remove('food');
+            }
+        })
+        food = null;
+    }
+
+    function playGame() {
+        if (food === null) {
+            foodGenerator();
+        }
+        else {
+            if (snake[0].x == food.x && snake[0].y == food.y) {
+                const tail = { x: snake[0].x, y: snake[0].y };
+                // head = { x: food.x, y: food.y };
+                snake.push(tail);
+                // snake.unshift(head);
+                clearFood();
+            }
+        }
+    }
+    function gameLoop() {
+        let check = 1;
+        const gameLoopId = setInterval(() => {
+            moveSnake()
+            drawSnake()
+            playGame()
+            console.log(snake[0].x, snake[0].y)
+            if (direction === "right" && snake[0].y > cols - 1) {
+                clearInterval(gameLoopId);
+                gameOver()
+            }
+            else if (direction === "left" && snake[0].y < 0) {
+                clearInterval(gameLoopId);
+                gameOver()
+            }
+            else if (direction === "down" && snake[0].x > rows - 1) {
+                clearInterval(gameLoopId);
+                gameOver()
+            }
+            else if (direction === "up" && snake[0].x < 0) {
+                clearInterval(gameLoopId);
+                gameOver()
+            }
+        }, 400);
+    }
+
+    document.querySelector(".start-btn").addEventListener("click", () => {
+        document.querySelector(".start-overlay").style.transform = "scale(0)";
+        document.querySelector(".start-overlay").style.transition = "0.5s";
+        // document.querySelector(".start-overlay").style.display = "none";
+        setTimeout(gameLoop, 600)
+        bgm.play()
     })
-}
 
-function moveSnake() {
-    const cells = document.querySelectorAll('.block');
-    let head = null;
-    if (direction === 'right') {
-        head = { x: snake[0].x, y: snake[0].y + 1 };
-        snake.forEach((part) => {
-            cells.forEach((cell) => {
-                if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
-                    cell.classList.remove('snake');
-    
-                }
-            })
-        })
+
+
+    function gameOver() {
+        bgm.pause();
+        document.querySelector(".over-overlay").style.transform = "scale(1)";;
+        document.querySelector(".over-overlay").style.transition = "0.5s";
+        over.play()
+        console.log("game over")
     }
+    document.querySelector(".replay-btn").addEventListener("click", () => {
+        const cells = document.querySelectorAll('.block');
+        cells.forEach((cell) => {
+            cell.classList.remove('snake');
 
-    else if (direction === 'left') {
-        head = { x: snake[0].x, y: snake[0].y - 1 };
-        snake.forEach((part) => {
-            cells.forEach((cell) => {
-                if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
-                    cell.classList.remove('snake');
-                }
-            })
         })
-    }
 
-    if (direction === 'up') {
-        head = { x: snake[0].x - 1, y: snake[0].y };
-        snake.forEach((part) => {
-            cells.forEach((cell) => {
-                if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
-                    cell.classList.remove('snake');
-                }
-            })
-        })
-    }
+        snake = [{ x: 1, y: 3 }];
 
-    else if (direction === 'down') {
-        head = { x: snake[0].x + 1, y: snake[0].y };
-        snake.forEach((part) => {
-            cells.forEach((cell) => {
-                if (cell.getAttribute('data-row') == part.x && cell.getAttribute('data-col') == part.y) {
-                    cell.classList.remove('snake');
-                }
-            })
-        })
-    }
 
-    snake.unshift(head);
-    snake.pop();
-    
-}
 
-function clearFood() {
-    const cells = document.querySelectorAll('.block');
-    cells.forEach((cell) => {
-        if (cell.getAttribute('data-row') == food.x && cell.getAttribute('data-col') == food.y) {
-            document.querySelector('.food').style.backgroundImage = "";
+        cells.forEach((cell) => {
+            const foodCell = document.querySelector('.food');
+            if (foodCell) foodCell.style.backgroundImage = "";
+
             cell.classList.remove('food');
-        }
+
+        })
+        food = null;
+        document.querySelector(".over-overlay").style.transform = "scale(0)";
+        document.querySelector(".over-overlay").style.transition = "0.5s";
+        direction = "right";
+        setTimeout(gameLoop, 600)
+        bgm.play();
     })
-    food = null;
-}
-
-function playGame() {
-    if (food === null) {
-        foodGenerator();
-    } 
-    else {
-        if (snake[0].x == food.x && snake[0].y == food.y) {
-            const tail = { x: snake[0].x, y: snake[0].y };
-            // head = { x: food.x, y: food.y };
-            snake.push(tail);
-            // snake.unshift(head);
-            clearFood();
-        } 
-    }
-}
-function gameLoop() {
-    let check=1;
-    const gameLoopId = setInterval(() => {
-        moveSnake()
-        drawSnake()
-        playGame()
-    }, 400);
-}
-
-document.querySelector(".start-btn").addEventListener("click",()=>{
-    document.querySelector(".overlay").style.scale=0;
-    document.querySelector(".overlay").style.transition="0.5s";
-    setTimeout(gameLoop, 600)
-})
+});
